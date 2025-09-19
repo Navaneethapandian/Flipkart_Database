87013005455372acbf7f7897cc27e06871c46f8a
T.S. Navaneethapandian <your_github_email@example.com>
2025-09-19 12:52:01 +0530

    updated feature / bug fix / small change

--- config/email.js
+++ config/email.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config(); // fixed
+require('dotenv').config(); 
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.MY_EMAIL,
-        pass: process.env.MY_PASS, // use App Password
+        pass: process.env.MY_PASS,
     }
 });
 
